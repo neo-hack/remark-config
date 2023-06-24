cf5f53f5ea8e31b54a9a170345bf82efb8d2be54
JW <jiangweixian1994@gmail.com>
2023-06-24 23:21:12 +0800

    :recycle:  node16

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,17 @@
-import { welcome } from '../src'
-import { describe, test, expect, vi } from 'vitest'
+import { remark } from 'remark'
+import { reporter } from 'vfile-reporter'
+import { test } from 'vitest'
 
-describe('index', () => {
-  test('demo part', () => {
-    console.log = vi.fn()
-    welcome()
-    expect(console.log).toHaveBeenCalledWith('hello world')
-  })
+import config from '../src/index'
+
+async function main() {
+  const file = await remark()
+    .use(config)
+    .process('1) Hello, _Jupiter_ and *Neptune*!')
+
+  console.error(reporter(file as any))
+}
+
+test('basic', () => {
+  main()
 })
