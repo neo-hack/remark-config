46d73616bd14f556a63841537a1cd4f96df29523
JW <jiangweixian1994@gmail.com>
2023-06-25 22:34:26 +0800

    :see_no_evil:  ignore changelog

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 export default {
   "**/**/*.{js,ts,tsx,vue,json,md}": ["eslint --fix"],
   // Lint Markdown files with remark
-  '**/**/*.md': filenames => [
+  '**/**/!(*CHANGELOG).md': filenames => [
     `remark ${filenames.join(' ')} -o`,
   ],
 }
